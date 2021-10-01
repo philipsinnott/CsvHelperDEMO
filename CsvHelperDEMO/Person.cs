33b8fff716f42dc06eb668cd59294546525482d5
Philip Sinnott <philipsinnotts@gmail.com>
2021-10-01 13:06:12 +0200

    Initial commit

---
+++ CsvHelperDEMO/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+
+namespace CsvHelperDEMO
+{
+    public class Person
+    {
+        //[Name("Username")]
+        public string Username { get; set; }
+        //[Name("FirstName")]
+        public string FirstName { get; set; }
+        //[Name("LastName")]
+        public string LastName { get; set; }
+        //[Name("Email")]
+        public string Email { get; set; }
+    }
+}
---
+++ CsvHelperDEMO/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using System.Linq;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Collections.Generic;
+
+namespace CsvHelperDEMO
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            List<Person> records = new List<Person>();
+            using (var streamReader = new StreamReader(@"C:\Users\Phili\source\repos\CsvHelperDEMO\CsvHelperDEMO\bin\Debug\net5.0\klasslista-21.csv"))
+            {
+                var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    Delimiter = ";"
+                };
+
+                using (var csvReader = new CsvReader(streamReader, config))
+                {
+                    records = csvReader.GetRecords<Person>().ToList();
+                }
+                foreach (var record in records)
+                {
+                    Console.WriteLine($"{record.Username} | {record.FirstName} {record.LastName} | Mail: {record.Email}");
+                }
+            }
+        }
+    }
+}
